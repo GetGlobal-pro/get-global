1f8ca85accbb8addc37e299777b1c695a0ba0d37
Nmn <nssachdeva2@gmail.com>
2024-01-08 00:59:18 +0530

    resolution fix (scaled to 0.8)

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
+"use client";
+
 import HomePage from "../components/Home/Home";
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    // Set the zoom level
+    document.body.style.zoom = '80%';
+  }, []);
+
   return (
     <main className="w-full h-full min-h-screen bg-white-main sm:bg-[url('/Assets/MainBg.png')] sm:bg-cover sm:bg-center sm:bg-no-repeat">
       <HomePage />
