c55351abb5b8fce322529bc99ba9625476ab9a86
Nmn <nssachdeva2@gmail.com>
2024-01-08 00:12:55 +0530

    Changed plausible tracking to main domain

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <PlausibleProvider domain="get-global-beta.vercel.app"
+        <PlausibleProvider domain="getglobal.pro"
           // trackLocalhost={true}
           // enabled={true}
           taggedEvents={true}
